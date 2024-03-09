bbfb7c6f7a91fc7a59ac94c5f60bc4b63dd44bc6
maaxx-m <161996227+maaxx-m@users.noreply.github.com>
2024-03-09 17:35:07 +0900

    rename caption

--- 트게더 링크 변경.user.js
+++ 트게더 링크 변경.user.js
@@ -3,7 +3,7 @@
 // @name:ko      트게더 링크 변경
 // @name:en      TGD Twtich Link to Chzzk
 // @namespace    http://tampermonkey.net/
-// @version      2024-03-09-debugging-1731
+// @version      2024-03-09-debugging-1735
 // @description  (여까)트게더의 트위치링크를 치지직 링크로 변경
 // @author       Maaxx
 // @match        *https://tgd.kr/s/yeokka*
@@ -57,7 +57,7 @@ for (var i = 0; i < tgdScripts.length; i++) {
 	$('body').append(appendScript);
 }
 
-/* main script */
+/* change link to chzzk */
     $('head').append(cssChzzk);
     $('#board-info-bottom a:nth-child(3)').attr('href', 'https://chzzk.naver.com/3c9fe16c70cf2f4a5274fa69307f0f89');
 }
