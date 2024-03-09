1bda6101e9f3a57a4ec51f0df64bd546a8deb677
maaxx-m <161996227+maaxx-m@users.noreply.github.com>
2024-03-09 14:52:47 +0900

    fix 'use strict'

--- 트게더 링크 변경.user.js
+++ 트게더 링크 변경.user.js
@@ -3,7 +3,7 @@
 // @name:ko      트게더 링크 변경
 // @name:en      TGD Twtich Link to Chzzk
 // @namespace    http://tampermonkey.net/
-// @version      2024-03-09-debugging-1426
+// @version      2024-03-09-debugging-1452
 // @description  (여까)트게더의 트위치링크를 치지직 링크로 변경
 // @author       Maaxx
 // @match        *https://tgd.kr/s/yeokka*
@@ -20,7 +20,7 @@ s.innerHTML = data;
 head.appendChild(s);
 // if page is fully loaded execute your program
 $(document).ready(function() {
-'use strict'
+'use strict';
 /* main scripts */
 //add css to link
     var cssChzzk=`
